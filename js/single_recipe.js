127216a50243b4ab22d01a0abce2b83e35537ce0
Hetizz <heta.huostila@metropolia.fi>
2021-12-15 22:30:27 +0200

    single recipe ready

--- js/single_recipe.js
+++ js/single_recipe.js
@@ -15,8 +15,16 @@ console.log(recipe_id);
 const resepti = document.querySelector('#tiedot');
 const ainekset = document.querySelector('#ainekset');
 const ohjeet = document.querySelector('#ohjeet');
+const kuva = document.querySelector('#kuva');
 
 const createRecipe = (recipe) => {
+  const img = document.createElement('img');
+  img.src = url + recipe.recipe_image.small;
+  img.alt = 'ruokakuva';
+  kuva.appendChild(img);
+  console.log(recipe.recipe_image.small);
+
+
   const h1 = document.createElement('h1');
   h1.innerHTML = recipe.title;
   resepti.appendChild(h1);
@@ -29,14 +37,25 @@ const createRecipe = (recipe) => {
   time.innerHTML = 'Valmistusaika: ' + recipe.time + ' min';
   resepti.appendChild(time);
 
-  const ul = document.createElement('ul');
+  const ingr = document.createElement('ul');
   for (let i = 0; i < recipe.ingredients.length; i++) {
     console.log(recipe.ingredients[i]);
     let li = document.createElement('li');
     li.innerHTML = recipe.ingredients[i];
-    ul.appendChild(li);
+    ingr.appendChild(li);
   }
-  ainekset.appendChild(ul);
+  ainekset.appendChild(ingr);
+
+  const kissa = document.createElement('ol');
+  for (let i = 0; i < recipe.directions.length; i++) {
+    console.log(recipe.directions[i]);
+    let li = document.createElement('li');
+    li.innerHTML = recipe.directions[i];
+    kissa.appendChild(li);
+  }
+  console.log(kissa);
+  ohjeet.appendChild(kissa);
+
 };
 
 const getRecipes = async () => {
